a15d8557b8ca4c9b22c82b3835b4a419c13cf79a
Admin <makwanadharmishtha44@gmail.com>
2022-12-21 21:26:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:cuntry/home.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      )
+  );
+}
